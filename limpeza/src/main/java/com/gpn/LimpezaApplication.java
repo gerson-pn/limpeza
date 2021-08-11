085e6ba6a7dde6d4c84a64dc0d3c13fe937b4aa9
Penha <Penha@DESKTOP-T7547TI>
2021-08-11 13:04:06 -0300

    inclusão do projeto limpeza

---
+++ limpeza/src/main/java/com/gpn/LimpezaApplication.java
@@ -0,0 +1,19 @@
+package com.gpn;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimpezaApplication {
+
+	public static void main(String[] args) throws IOException {
+		/*
+		 * Início da aplicação com o spring boot
+		 * */
+		SpringApplication.run(LimpezaApplication.class, args);
+		
+	}
+
+}
---
+++ limpeza/src/test/java/com/gpn/LimpezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimpezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
